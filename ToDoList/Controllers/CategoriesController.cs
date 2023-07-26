24206369f029115e969dc5356848017c4a8f24aa
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-26 13:42:34 -0700

    Add Categories index route and page

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -2,12 +2,24 @@ using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class CategoriesController : Controller
   {
+    private readonly ToDoListContext _db;
 
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
     /* [HttpGet("/categories")] */
     /* public ActionResult Index() */
     /* { */
