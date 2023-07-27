ed483899ac7bc4fea45ed29dddd027d61a1371c6
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-27 09:31:55 -0700

    Create function and UI to group items into categories

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ToDoList.Models;
 using System.Collections.Generic;
@@ -26,12 +27,17 @@ namespace ToDoList.Controllers
 
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
     [HttpPost]
     public ActionResult Create(Item item)
     {
+      if (item.CategoryId == 0)
+      {
+        return RedirectToAction("Create");
+      }
       _db.Items.Add(item);
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -41,13 +47,16 @@ namespace ToDoList.Controllers
     {
       // queries the _db object's items list, finds the first that matches 
       // relevant id, then returns that item
-      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      Item thisItem = _db.Items
+                            .Include(item => item.Category)
+                            .FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
     
     public ActionResult Edit(int id)
     {
       Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View(thisItem);
     }
 
